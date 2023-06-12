b0a0d85f15c1e53c426d9659ac0112facbe61deb
SHAHIMA FIROZ KHAN <99420269+Shahimafiroz@users.noreply.github.com>
2023-06-12 18:57:32 +0530

    objects and classes

--- array/object&ckasses.js
+++ array/object&ckasses.js
@@ -1,18 +1,21 @@
 const str = ["a", "b", "c", "d"];
 // 4*4 = 16 bytes
+
+//lookup using index 
 str[2];
-console.log(str[2]);
-// push
-str.push("e"); //O(1)            Add the element to the last
+console.log(str[2]); // O(1)                                  View elements at the given index
+// push (insert )
+str.push("e"); //O(1)                                         Add the element to the last
 
-//pop
-str.pop(); //O(1)                Removes the last element
+//pop (delete)
+str.pop(); //O(1)                                             Removes the last element
 
-//unshift
-str.unshift("x"); // O(n)        Adds the element at the begning of the array
+//unshift (insert)
+str.unshift("x"); // O(n)                                     Adds the element at the begning of the array
 
-//Splice                         inserts element in between
-str.splice(4, 0, "Shahima"); //  array.splice( index , elemenntsToBeDeleted , Item_to_be_added)
+//Splice:-  (inserts & delete) element in between            array.splice( index , elemenntsToBeDeleted , Item_to_be_added)           
+
+str.splice(4, 0, "Shahima"); //  // O(n/2)---> but ignore constants acc to rules .: O(n)  
 
 //
 console.log(str);
--- genral.js
+++ genral.js
@@ -1,3 +1,25 @@
-function shhh(n) {
- 
+// REFRENCE TYPES
+
+let obj1 = { value: 10 };
+let obj2 = obj1; // obj2 references the same object as obj1
+obj2.value = 20;
+console.log(obj1.value); // Output: 20
+
+//CONTEXT
+
+// In the above example, the greet function is defined within the obj object. When called as obj.greet(), the this keyword inside the function refers to the obj object. The name property of obj is accessed within the function due to the context provided by the object method call. In contrast, when the sayHello function is called without any specific object context, this refers to the global object. Since there is no name property defined in the global object, the output is undefined. Here, the scope is the global scope, and the context is the global context.
+const obj = {
+  name: "John",
+  greet: function () {
+    console.log(`Hello, ${this.name}!`);
+  },
+};
+
+function sayHello() {
+  console.log(`Hello, ${this.name}!`);
 }
+
+obj.greet(); // Output: Hello, John! (context is obj, scope is the object method)
+sayHello(); // Output: Hello, undefined! (context is global, scope is the global scope)
+
+// INSTANTIATION
